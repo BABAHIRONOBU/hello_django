5ebe578963cc9272b302aea3750aef7057e059e1
BABA HIRONOBU <hironobu@BABAui-MacBook-Pro.local>
2018-10-20 21:42:14 +0900

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hironobu/Documents/Github/hello_world/hello_django/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
